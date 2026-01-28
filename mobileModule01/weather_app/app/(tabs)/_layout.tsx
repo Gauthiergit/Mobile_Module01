ef69bc712bbe61c91c79e6362d35a6e1e7012862
Gauthiergit <g.p220498@gmail.com>
2026-01-28 18:36:04 +0100

    wrapper

--- mobileModule01/weather_app/app/(tabs)/_layout.tsx
+++ mobileModule01/weather_app/app/(tabs)/_layout.tsx
@@ -6,6 +6,7 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import { withLayoutContext } from 'expo-router';
 import { SearchLocalisationProvider } from '@/providers/SearchLocalisationProvider';
 import { Header } from '@/components/Header';
+import { PlatformWrapper } from '@/wrappers/PlatformWrapper';
 
 export default function TabLayout() {
 
@@ -15,43 +16,41 @@ export default function TabLayout() {
 
 	return (
 		<SearchLocalisationProvider>
-			<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-				<View style={{flex: 1}}>
-					<Header/>
-					<MaterialTopTabs
-						tabBarPosition="bottom"
-						screenOptions={{
-						tabBarActiveTintColor: tintColor,
-						tabBarInactiveTintColor: 'gray',
-						tabBarLabelStyle: { fontSize: 20, textTransform: 'none' },
-						tabBarIndicatorStyle: { backgroundColor: tintColor, height: 2, top: 0 },
-						tabBarStyle: { backgroundColor: Colors[colorScheme ?? "light"].background },
-						swipeEnabled: true,
-					}}>
-						<MaterialTopTabs.Screen
-							name="index" 
-							options={{
-								title: 'Currently',
-								tabBarIcon: ({ color }: { color: string }) => <MaterialCommunityIcons name="weather-sunset" size={30} color={color} />,
-							}}
-						/>
-						<MaterialTopTabs.Screen
-							name="today"
-							options={{
-								title: 'Today',
-								tabBarIcon: ({ color }: { color: string }) => <MaterialCommunityIcons name="weather-partly-cloudy" size={30} color={color} />,
-							}}
-						/>
-						<MaterialTopTabs.Screen
-							name="weekly"
-							options={{
-								title: 'Weekly',
-								tabBarIcon: ({ color }: { color: string }) => <MaterialCommunityIcons name="weather-tornado" size={30} color={color} />,
-							}}
-						/>
-					</MaterialTopTabs>
-				</View>
-			</TouchableWithoutFeedback>
+			<PlatformWrapper>
+				<Header/>
+				<MaterialTopTabs
+					tabBarPosition="bottom"
+					screenOptions={{
+					tabBarActiveTintColor: tintColor,
+					tabBarInactiveTintColor: 'gray',
+					tabBarLabelStyle: { fontSize: 20, textTransform: 'none' },
+					tabBarIndicatorStyle: { backgroundColor: tintColor, height: 2, top: 0 },
+					tabBarStyle: { backgroundColor: Colors[colorScheme ?? "light"].background },
+					swipeEnabled: true,
+				}}>
+					<MaterialTopTabs.Screen
+						name="index" 
+						options={{
+							title: 'Currently',
+							tabBarIcon: ({ color }: { color: string }) => <MaterialCommunityIcons name="weather-sunset" size={30} color={color} />,
+						}}
+					/>
+					<MaterialTopTabs.Screen
+						name="today"
+						options={{
+							title: 'Today',
+							tabBarIcon: ({ color }: { color: string }) => <MaterialCommunityIcons name="weather-partly-cloudy" size={30} color={color} />,
+						}}
+					/>
+					<MaterialTopTabs.Screen
+						name="weekly"
+						options={{
+							title: 'Weekly',
+							tabBarIcon: ({ color }: { color: string }) => <MaterialCommunityIcons name="weather-tornado" size={30} color={color} />,
+						}}
+					/>
+				</MaterialTopTabs>
+			</PlatformWrapper>
 		</SearchLocalisationProvider>
 	);
 }
